5366025dceaa800a8fcfb7d4818ea4fcf68c0f3c
Kasshabz <142917268+Kasshabz@users.noreply.github.com>
2024-02-20 09:56:17 +0000

    post shop test

--- __tests__/app.test.ts
+++ __tests__/app.test.ts
@@ -52,11 +52,11 @@ describe("POST /users", () => {
       age: 40,
       email: "moe@example.com",
       avatar_url: "http://example.com/avatar11.jpg",
-        };
+    };
     const res = await request(app).post("/users").send(userBody);
     expect(res.status).toBe(201);
   });
-  test('should not create a user with a duplicate email', async () => {
+  test("should not create a user with a duplicate email", async () => {
     const userBody = {
       name: "Mohammed Ali",
       age: 40,
@@ -65,12 +65,28 @@ describe("POST /users", () => {
       coffee_count: 0,
     };
 
-
-    await request(app).post('/users').send(userBody);   
-    const response = await request(app).post('/users').send(userBody);
+    await request(app).post("/users").send(userBody);
+    const response = await request(app).post("/users").send(userBody);
 
     expect(response.status).toBe(400);
     expect(response.body.code).toBe(11000);
   });
-  
+});
+describe("POST /Shops", () => {
+  test("Should add a new shop to database", async () => {
+    const shopBody = {
+      name: "Probeans",
+      lat: 53.4808,
+      long: 2.2426,
+      description: "High energy coffee shop",
+      email: "ProBeans@example.com",
+      avatar_url: "http://example.com/avatar11.jpg",
+    };
+    const res = await request(app).post("/shops").send(shopBody);
+    console.log(res.body, "line 85");
+    expect(res.status).toBe(201);
+    expect(typeof res.body.shop.name).toBe("string");
+    expect(typeof res.body.shop.email).toBe("string");
+    expect(typeof res.body.shop.location.lat).toBe("number");
+  });
 });
--- src/models/shops-models.ts
+++ src/models/shops-models.ts
@@ -2,39 +2,47 @@ import { connectDatabase, client } from "../dbConnection";
 import { Db } from "mongodb";
 let db: Db;
 connectDatabase().then((database) => {
-    db = database;
+  db = database;
 });
 
 export const fetchShops = async () => {
-    const shops = await db.collection("CoffeeShops").find({}).toArray();
+  const shops = await db.collection("CoffeeShops").find({}).toArray();
 
-    return shops;
+  return shops;
 };
 
 interface Shop extends Object {
-    name?: string,
-    email: string,
-    avatar_url?: string,
-    lat?: number,
-    long?: number,
-    description?:string
-
+  name?: string;
+  email: string;
+  avatar_url?: string;
+  lat?: number;
+  long?: number;
+  description?: string;
 }
 export const insertShop = async (shop: Shop) => {
-    try {
-        const newShop = await db.collection("CoffeeShops").insertOne({
+  try {
+    const newShop = await db.collection("CoffeeShops").insertOne({
+      name: shop.name,
+      email: shop.email,
+      avatar_url: shop.avatar_url,
+      location: {
+        lat: shop.lat,
+        long: shop.long,
+      },
+      description: shop.description,
+    });
+    let data: any = {};
 
-            name: shop.name,
-            email: shop.email,
-            avatar_url: shop.avatar_url,
-            location: {
-                lat: shop.lat,
-                long: shop.long,
-            },
-            description: shop.description,
-        });
-
-        return newShop;
+    if (newShop.acknowledged) {
+      // ... prepare the data
+      data = await db
+        .collection("CoffeeShops")
+        .findOne({ _id: newShop.insertedId });
+      console.log(data, "line 42");
     }
-    catch (error) { throw error }
+
+    return data;
+  } catch (error) {
+    throw error;
+  }
 };
